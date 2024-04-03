b1641ac27e57e508ecc24c7456d17c29131c3dec
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-04-03 19:46:10 -0300

    feat: add Component Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,

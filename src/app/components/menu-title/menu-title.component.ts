249f6a0b3a10cc2ca68f0914574b494f2cd9b8ee
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-04-05 10:36:07 -0300

    feat: Add Header and Menu Components

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrl: './menu-title.component.css'
+  styleUrls: ['./menu-title.component.css'], // Corrigido para styleUrls
 })
 export class MenuTitleComponent {
-
+  menuItems: MenuItem[] = [
+    { title: 'Home', route: '/home' },
+    { title: 'Menu', route: '/cafes' },
+    { title: 'Local', route: '/local' },
+  ];
 }
